aa72843f59f08faf30489ae9ea401473c82c73c8
Timshel <Timshel@users.noreply.github.com>
2024-09-12 14:50:00 +0200

    Icons and rename to OIDCWarden

--- apps/web/src/app/oss-routing.module.ts
+++ apps/web/src/app/oss-routing.module.ts
@@ -184,7 +184,7 @@ const routes: Routes = [
         canActivate: [unauthGuardFn()],
         data: {
           pageTitle: {
-            key: "logInToBitwarden",
+            key: "logIn",
           },
           pageIcon: VaultIcon,
         } satisfies RouteDataProperties & AnonLayoutWrapperData,
@@ -325,9 +325,6 @@ const routes: Routes = [
             key: "singleSignOn",
           },
           titleId: "enterpriseSingleSignOn",
-          pageSubtitle: {
-            key: "singleSignOnEnterOrgIdentifierText",
-          },
           pageIcon: SsoKeyIcon,
         } satisfies RouteDataProperties & AnonLayoutWrapperData,
         children: [

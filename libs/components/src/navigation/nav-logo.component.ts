896a16a66337846b6f102b84747c8f8fd20f7665
Timshel <Timshel@users.noreply.github.com>
2024-09-12 14:50:00 +0200

    Icons and rename to OIDCWarden

--- libs/components/src/navigation/nav-logo.component.ts
+++ libs/components/src/navigation/nav-logo.component.ts
@@ -1,29 +1,24 @@
+import { CommonModule } from "@angular/common";
 import { NgClass } from "@angular/common";
 import { ChangeDetectionStrategy, Component, input, inject } from "@angular/core";
 import { RouterLinkActive, RouterLink } from "@angular/router";
 
-import { BitwardenShield, BitSvg } from "@bitwarden/assets/svg";
-
-import { SvgComponent } from "../svg/svg.component";
+import { BitSvg } from "@bitwarden/assets/svg";
 
 import { SideNavService } from "./side-nav.service";
 
 @Component({
   selector: "bit-nav-logo",
   templateUrl: "./nav-logo.component.html",
-  imports: [NgClass, RouterLinkActive, RouterLink, SvgComponent],
+  imports: [CommonModule, NgClass, RouterLinkActive, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: "tw-contents" },
 })
 export class NavLogoComponent {
   protected readonly sideNavService = inject(SideNavService);
 
-  /**
-   * Icon that is displayed when the side nav is closed
-   *
-   * @default BitwardenShield
-   */
-  readonly closedIcon = input(BitwardenShield);
+  /** Icon that is displayed when the side nav is closed */
+  readonly closedIcon = input.required<string>();
 
   /**
    * Icon that is displayed when the side nav is open

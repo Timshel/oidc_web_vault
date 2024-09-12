aa8243c6bf38f70997d9aa09fe6452d35466c0da
Timshel <Timshel@users.noreply.github.com>
2024-09-12 14:50:00 +0200

    Icons and rename to OIDCWarden

--- apps/web/src/app/oss-routing.module.ts
+++ apps/web/src/app/oss-routing.module.ts
@@ -245,7 +245,7 @@ const routes: Routes = [
         canActivate: [unauthGuardFn()],
         data: {
           pageTitle: {
-            key: "loginPageEmailEntryScreenTitle",
+            key: "logIn",
           },
           pageIcon: VaultIcon,
         } satisfies RouteDataProperties & AnonLayoutWrapperData,
@@ -388,9 +388,6 @@ const routes: Routes = [
             key: "singleSignOn",
           },
           titleId: "enterpriseSingleSignOn",
-          pageSubtitle: {
-            key: "singleSignOnEnterOrgIdentifierText",
-          },
           pageIcon: SsoKeyIcon,
         } satisfies RouteDataProperties & AnonLayoutWrapperData,
         children: [
--- libs/components/src/navigation/nav-logo.component.ts
+++ libs/components/src/navigation/nav-logo.component.ts
@@ -1,34 +1,24 @@
+import { CommonModule } from "@angular/common";
 import { NgClass } from "@angular/common";
 import { ChangeDetectionStrategy, Component, input, inject } from "@angular/core";
 import { RouterLinkActive, RouterLink } from "@angular/router";
 
-import { BitwardenShield, SideNavLogo, BitSvg } from "@bitwarden/assets/svg";
-
-import { SvgComponent } from "../svg/svg.component";
+import { BitSvg } from "@bitwarden/assets/svg";
 
 import { SideNavService } from "./side-nav.service";
 
 @Component({
   selector: "bit-nav-logo",
   templateUrl: "./nav-logo.component.html",
-  imports: [NgClass, RouterLinkActive, RouterLink, SvgComponent],
+  imports: [CommonModule, NgClass, RouterLinkActive, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: "tw-contents" },
 })
 export class NavLogoComponent {
   protected readonly sideNavService = inject(SideNavService);
 
-  /**
-   * The v2 side nav always displays this logo for the open state, regardless of `openIcon`.
-   */
-  protected readonly sideNavLogo = SideNavLogo;
-
-  /**
-   * Icon that is displayed when the side nav is closed
-   *
-   * @default BitwardenShield
-   */
-  readonly closedIcon = input(BitwardenShield);
+  /** Icon that is displayed when the side nav is closed */
+  readonly closedIcon = input.required<string>();
 
   /**
    * Icon that is displayed when the side nav is open

d9b8471c3054e0a3d0bdf9710f1413d42dee5e24
Timshel <timshel@users.noreply.github.com>
2025-04-03 18:36:25 +0200

    ServerSettings to toggle SSO/MP buttons

--- libs/common/src/platform/models/domain/server-settings.ts
+++ libs/common/src/platform/models/domain/server-settings.ts
@@ -1,9 +1,13 @@
 export class ServerSettings {
   disableUserRegistration: boolean;
   suppressOnboardingInterstitials: boolean;
+  ssoEnabled: boolean;
+  ssoOnly: boolean;
 
   constructor(data?: Partial<ServerSettings>) {
     this.disableUserRegistration = data?.disableUserRegistration ?? false;
     this.suppressOnboardingInterstitials = data?.suppressOnboardingInterstitials ?? false;
+    this.ssoEnabled = data?.ssoEnabled ?? true;
+    this.ssoOnly = data?.ssoOnly ?? false;
   }
 }

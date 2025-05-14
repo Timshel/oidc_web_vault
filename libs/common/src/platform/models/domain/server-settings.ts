d59fab71f1f92b10bb04275aa6742ee7804d76b7
Timshel <timshel@users.noreply.github.com>
2025-05-14 18:43:03 +0200

    ExternalId on Org and Group for SSO

--- libs/common/src/platform/models/domain/server-settings.ts
+++ libs/common/src/platform/models/domain/server-settings.ts
@@ -2,10 +2,14 @@ export class ServerSettings {
   disableUserRegistration: boolean;
   ssoEnabled: boolean;
   ssoOnly: boolean;
+  ssoOrgExternalId: boolean;
+  ssoOrgGroupExternalId: boolean;
 
   constructor(data?: ServerSettings) {
     this.disableUserRegistration = data?.disableUserRegistration ?? false;
     this.ssoEnabled = data?.ssoEnabled ?? true;
     this.ssoOnly = data?.ssoOnly ?? false;
+    this.ssoOrgExternalId = data?.ssoOrgExternalId ?? false;
+    this.ssoOrgGroupExternalId = data?.ssoOrgGroupExternalId ?? false;
   }
 }

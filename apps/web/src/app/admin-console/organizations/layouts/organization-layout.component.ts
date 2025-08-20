2a0fabb1bce91025879f1b16d86e972e383abfb6
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-08-20 03:45:59 +0200

    [vw] remove integrations
    because this currently depends on proprietary code

--- apps/web/src/app/admin-console/organizations/layouts/organization-layout.component.ts
+++ apps/web/src/app/admin-console/organizations/layouts/organization-layout.component.ts
@@ -60,8 +60,6 @@ export class OrganizationLayoutComponent implements OnInit {
 
   protected orgFilter = (org: Organization) => canAccessOrgAdmin(org);
 
-  protected integrationPageEnabled$: Observable<boolean>;
-
   organization$: Observable<Organization>;
   canAccessExport$: Observable<boolean>;
   showPaymentAndHistory$: Observable<boolean>;
@@ -131,8 +129,6 @@ export class OrganizationLayoutComponent implements OnInit {
           provider.providerStatus !== ProviderStatusType.Billable,
       ),
     );
-
-    this.integrationPageEnabled$ = this.organization$.pipe(map((org) => org.canAccessIntegrations));
 
     this.subscriber$ = this.organization$.pipe(
       map((organization) => ({

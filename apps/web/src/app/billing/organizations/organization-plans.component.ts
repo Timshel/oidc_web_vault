d8e78f00a9cd17a5f8f92241cbb9e5aeff8f4f9e
Stefan Melmuk <stefan.melmuk@gmail.com>
2025-12-30 21:43:55 +0100

    [VW] skip validation of billing forms

--- apps/web/src/app/billing/organizations/organization-plans.component.ts
+++ apps/web/src/app/billing/organizations/organization-plans.component.ts
@@ -782,12 +782,13 @@ export class OrganizationPlansComponent implements OnInit, OnDestroy {
     }
 
     // Validate billing form for paid plans during creation
+    /* don't validate in Vaultwarden because we have no plan selected
     if (this.createOrganization() && this.selectedPlan()?.type !== PlanType.Free) {
       this.billingFormGroup.markAllAsTouched();
       if (this.billingFormGroup.invalid) {
         return;
       }
-    }
+    } */
 
     const doSubmit = async (): Promise<string> => {
       let orgId: string;
